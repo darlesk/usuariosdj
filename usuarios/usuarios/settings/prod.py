466366028b7ce7641a808cb6f381dc0859ced8f4
darlesk <ahiezer.rabadan3@gmail.com>
2023-01-17 12:31:47 -0600

    Se cambio el archivo settings/prod

--- usuarios/usuarios/settings/prod.py
+++ usuarios/usuarios/settings/prod.py
@@ -6,14 +6,12 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 # Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dbbiblioteca',
-        'USER':'ahiezer',
-        'PASSWORD':'ASDqwe123',
+        'NAME': get_secret('DB_NAME'),
+        'USER':get_secret('USER'),
+        'PASSWORD':get_secret('PASSWORD'),
         'HOST':'localhost',
         'PORT':'5432',
     }
